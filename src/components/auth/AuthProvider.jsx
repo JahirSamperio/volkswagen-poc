8801cfd5db5ea06216f1af205dfafe0631e93063
Jahir Alberto Samperio <jahirsampe1234@gmail.com>
2025-08-06 18:45:56 -0600

    Agregando https

--- src/components/auth/AuthProvider.jsx
+++ src/components/auth/AuthProvider.jsx
@@ -16,8 +16,8 @@ Amplify.configure({
         oauth: {
           domain: 'us-east-1bzgeloc9g.auth.us-east-1.amazoncognito.com',
           scopes: ['openid', 'email', 'phone'],
-          redirectSignIn: ['http://localhost:5173/dashboard'],
-          redirectSignOut: ['http://localhost:5173/'],
+          redirectSignIn: ['http://localhost:5173/dashboard', 'https://172.50.66.153/dashboard'],
+          redirectSignOut: ['http://localhost:5173/', 'https://172.50.66.153/'],
           responseType: 'code'
         }
       }
@@ -66,7 +66,7 @@ export const AuthProvider = ({ children }) => {
       await signOut();
       setUser(null);
       // Redirigir a la página de logout hospedada
-      window.location.href = 'https://us-east-1bzgeloc9g.auth.us-east-1.amazoncognito.com/logout?client_id=u41vq0son5fab0l4fnc8eusuv&logout_uri=http%3A%2F%2Flocalhost%3A5173%2F';
+      window.location.href = 'https://us-east-1bzgeloc9g.auth.us-east-1.amazoncognito.com/logout?client_id=u41vq0son5fab0l4fnc8eusuv&logout_uri=https%3A%2F%2F172.50.66.153%2F';
     } catch (error) {
       console.error('Error al cerrar sesión:', error);
       // Fallback: limpiar estado local y recargar
